7ebb0ec57a5d97345148fee878005ea862b506c2
Bruna <brunamnolasco@gmail.com>
2021-03-15 15:45:22 -0300

    fix: implement getStaticPaths

--- frontend/pages/restaurants/[id].js
+++ frontend/pages/restaurants/[id].js
@@ -1,16 +1,16 @@
 import { useRouter } from "next/router";
 import Head from "next/head";
 import Header from "../../src/components/Header";
-import restaurantsData from '../../public/data/restaurants'
+import restaurantsData from "../../public/data/restaurants"
 
-export default function Menu() {
+export default function Menu({data}) {
   const router = useRouter();
   const {
     query: { id },
   } = router;
 
-  const restaurant = restaurantsData.find((r) => r._id === id);
-
+  const restaurant = data.find((r) => r._id === id);
+  console.log(data)
   return (
     <>
       <Head>
@@ -24,4 +24,28 @@ export default function Menu() {
       <Header title={restaurant.name} />
     </>
   );
+}
+
+export async function getStaticPaths() {
+  const paths = [
+    { params: { id: "1"} },
+    { params: { id: "2"} },
+    { params: { id: "3"} },
+    { params: { id: "4"} },
+    { params: { id: "5"} },
+    { params: { id: "6"} },
+    { params: { id: "7"} },
+    { params: { id: "8"} }    
+  ]
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params}) {
+  const data = restaurantsData;
+
+  return { props: { data } };
 }
