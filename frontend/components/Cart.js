9b25e9e885ad8415ca3d175ff3e34c8ab0a00b07
Bruna <brunamnolasco@gmail.com>
2021-03-31 16:54:53 -0300

    style(Cart)

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -4,19 +4,32 @@ import styled from "styled-components";
 const CartWrapper = styled.aside`
   width: 60vw;
   height: min-content;
-  background-color: ${({ theme }) => theme.colors.background};
-  position: fixed;
+  background-color: rgba(255, 255, 255, 0.7);
+  backdrop-filter: blur(5px);
+  position: sticky;
   bottom: 0;
-  left: 50%;
-  transform: translate(-50%, 0);
+  display: block;
+  margin: 0 auto;
   padding: 1.3rem 2rem;
+  border: 1px solid #ffffff;
+  box-shadow: 0 0 5px #dddddd;
+  font: ${({ theme }) => theme.fonts.secLinks};
+
+  h4 {
+    font-size: 1.1rem;
+    margin-bottom: 20px;
+  }
 `;
 
 export default function Cart({ cartItems }) {
   return (
     <CartWrapper>
-      my Cart
-      {cartItems.length === 0 ? <p>No items in cart.</p> : null}
+      <h4>Carrinho</h4>
+      {cartItems.length === 0 ? (
+        <p>
+          Você ainda não adicionou nada. Clique sobre o item que deseja incluir!
+        </p>
+      ) : null}
       {cartItems.map((item) => (
         <CartItem key={item.id} item={item}>
           {item.option}
