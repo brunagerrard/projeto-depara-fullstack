3c2fd5b31f1cd8758b9ce9b3c448799d4b9ec500
Bruna Nolasco <brunamnolasco@gmail.com>
2021-06-05 22:15:48 -0300

    refactor: add favicon

--- pages/index.js
+++ pages/index.js
@@ -19,6 +19,7 @@ export default function Home() {
       <Head>
         <title>de_para</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;600&family=Roboto+Slab:wght@300;400;500;600&display=swap"
           rel="stylesheet"
