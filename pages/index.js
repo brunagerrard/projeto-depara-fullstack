04d9e183d17404c2c906764d44d2a813db78784b
Bruna M. Nolasco Santos de Carvalho <bnolasco@HYPERATIVA.NET>
2021-06-07 20:13:21 -0300

    feat: implemented next-auth with auth0

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+/** @format */
+
 import Head from 'next/head'
-import Nav from '../components/Nav'
+import { signIn, signOut, useSession } from 'next-auth/client'
 import styled from 'styled-components'
+import Nav from '../components/Nav'
 import Hero from '../components/Hero'
 
 const HomeBox = styled.div`
@@ -20,19 +23,39 @@ const HomeBox = styled.div`
   }
 `
 
+const Login = styled.div`
+  position: absolute;
+  top: 20px;
+`
+
 export default function Home() {
+  const [session, loading] = useSession()
   return (
     <HomeBox>
       <Head>
         <title>de_para: delivery camarada</title>
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link rel='shortcut icon' href='/favicon.png' />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <link
-          href='https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;600&family=Roboto+Slab:wght@300;400;500;600&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;600&family=Roboto+Slab:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
         />
       </Head>
-      <Hero/>
+      <Hero />
+      <Login>
+        {!session && (
+          <>
+            Not signed in <br />
+            <button onClick={() => signIn('auth0')}>Sign in</button>
+          </>
+        )}
+        {session && (
+          <>
+            Signed in as {session.user.email} <br />
+            <button onClick={() => signOut()}>Sign out</button>
+          </>
+        )}
+      </Login>
       <img
         src="/images/logodepara.svg"
         alt="Logotipo da aplicação de_para, com a ilustração de uma bicicleta formada por símbolos comunistas. O pneu dianteiro é similar à engrenagem de uma máquina, o traseiro é uma foice e o guidão o martelo."
