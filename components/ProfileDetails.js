6b8074fd2f70d2ab50d23e5b5a60245611dbf20a
Bruna M. Nolasco Santos de Carvalho <bnolasco@HYPERATIVA.NET>
2021-07-10 17:56:49 -0300

    feat: added react-tabs to the profile section

---
+++ components/ProfileDetails.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import styled from 'styled-components'
+
+const ProfileDetailsWrap = styled.aside`
+  
+`
+
+export default function ProfileDetails() {
+  return <ProfileDetailsWrap>profile</ProfileDetailsWrap>
+}
--- pages/profile.js
+++ pages/profile.js
@@ -5,14 +5,51 @@ import { useSession } from 'next-auth/client'
 import useSWR from 'swr'
 import styled from 'styled-components'
 import Session from '../components/Session'
-import Header from '../components/Header'
 import fetcher from '../utils/fetcher'
 import OrderHistory from '../components/OrderHistory'
+import ProfileDetails from '../components/ProfileDetails'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+import 'react-tabs/style/react-tabs.css'
+import { IoFastFoodOutline } from 'react-icons/io5'
+import { CgProfile } from 'react-icons/cg'
 
-const ProfileWrapper = styled.div`
+const ProfileWrap = styled.div`
   margin-top: 2rem;
-  padding: 1rem 10%;
+  padding: 2rem 5%;
+
+  .react-tabs__tab-panel {
+    background-color: #ffffff;
+    padding: 1rem;
+    border-radius: 0 0 10px 10px;
+  }
+
+  #tabs-list {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 0;
+    border: none;
+
+    .icon {
+      font-size: 1.4rem;
+    }
+
+    .react-tabs__tab {
+      width: 100%;
+      border: none;
+      border-radius: 10px 10px 0 0;
+    }
+
+    .react-tabs__tab--selected {
+      color: ${({ theme }) => theme.colors.primaryRed};
+      list-style: none;
+      p {
+        font-weight: bold;
+      }
+    }
+  }
 `
+
+const OrdersWrap = styled.div``
 
 export default function Profile() {
   const [session, loading] = useSession()
@@ -31,21 +68,41 @@ export default function Profile() {
     <>
       <Head>
         <title>{session?.user.email}</title>
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link rel='shortcut icon' href='/favicon.png' />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <link
-          href='https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;600&family=Roboto+Slab:wght@300;400;500;600&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;400;600&family=Roboto+Slab:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
         />
       </Head>
       <Session />
-      <Header title='Seu perfil' />
-      <ProfileWrapper>
-        {session && data && (
-          <p>oi, {data.data.name}! aqui estão seus últimos pedidos:</p>
-        )}
-        {userData && <OrderHistory userData={userData} />}
-      </ProfileWrapper>
+      <ProfileWrap>
+        <Tabs id="my-tabs">
+          <TabList id="tabs-list">
+            <Tab>
+              <p>Pedidos</p>
+              <IoFastFoodOutline className='icon' />
+            </Tab>
+            <Tab>
+              <p>Perfil</p>
+              <CgProfile className='icon' />
+            </Tab>
+          </TabList>
+
+          <TabPanel>
+            <OrdersWrap>
+              {session && data && (
+                <p>oi, {data.data.name}! aqui estão seus últimos pedidos:</p>
+              )}
+              {userData && <OrderHistory userData={userData} />}
+            </OrdersWrap>
+          </TabPanel>
+
+          <TabPanel>
+            <ProfileDetails />
+          </TabPanel>
+        </Tabs>
+      </ProfileWrap>
     </>
   )
 }
